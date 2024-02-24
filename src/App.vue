6513cd84290f5c1e1bdb010d0e493f9b9e0e3363
David wanjohi <davidwagura83@gmail.com>
2024-02-24 12:27:54 +0300

    did modifications on existing files

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,35 @@ import HelloWorld from './components/HelloWorld.vue'
   <header>
 
     <div class="wrapper">
+      
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <router-link class="navbar-brand" to="/">Navbar</router-link>
+          <router-link to="/">Home</router-link>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+              <li class="nav-item">
+                <router-link class="nav-link active"  to="/">Home</router-link>
+              </li>
+
+              <li class="nav-item">
+                <router-link class="nav-link" to="/about">About Us</router-link>
+              </li>
+
+              <li class="nav-item">
+                <router-link class="nav-link" to="/students">Students</router-link>
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
       </nav>
     </div>
   </header>
@@ -19,5 +44,4 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-
 </style>
