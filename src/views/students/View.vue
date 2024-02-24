db869fcad052eabe19c9cf716b56c01303021d57
David wanjohi <davidwagura83@gmail.com>
2024-02-24 14:06:13 +0300

    add students details display  table

--- src/views/students/View.vue
+++ src/views/students/View.vue
@@ -1,6 +1,54 @@
 <template>
-    <main>
-      <h1>This is student page</h1>
-    </main>
-  </template>
+    <div class="card">
+        <div class="card-header">
+            <h4>
+                Students
+                <router-link to="/student/create" class="btn btn-primary float-end">
+                    Add Student
+                </router-link>
+            </h4>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Gender</th>
+                    <th>Admission Number</th>
+                    <th>Home County</th>
+                    <th>Year Of Birth</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
   
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'students',
+    data() {
+        return {
+            students: []
+        }
+    },
+    mounted(){
+        this.getStudents();
+        // console.log('THE STUDENTS');
+    },
+    methods: {
+        getStudents() {
+            axios.get(`http://127.0.0.1:8000/api/students`).then( res => {  
+                console.log(res)
+            })
+        }
+    },
+}
+</script>
